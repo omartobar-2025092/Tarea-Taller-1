e9d8fa183f5ec055b728962a7ec508f1abd8202f
omartobar-2025092 <otobar-2025092@kinal.edu.gt>
2026-02-10 10:20:01 -0600

    feat: Se agrego la Entity Proveedores

---
+++ src/main/java/com/omartobar/RepuestosAutomotrices/entity/Proveedores.java
@@ -0,0 +1,49 @@
+package com.omartobar.RepuestosAutomotrices.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "Proveedores")
+public class Proveedores {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "idProveedor")
+    private Integer idProovedor;
+
+    @Column (name = "nombreProveedor")
+    private String nombreProveedor;
+
+    @Column (name = "telefonoProveedor")
+    private String telefonoProveedor;
+
+    @Column (name = "direccionProveedor")
+    private String direccionProveedor;
+
+    @Column (name = "emailProveedor")
+    private String emailProveedor;
+
+
+    //Getters y Setters//
+
+
+    public Integer getIdProovedor() {return idProovedor;}
+
+    public void setIdProovedor(Integer idProovedor) {this.idProovedor = idProovedor;}
+
+    public String getNombreProveedor() {return nombreProveedor;}
+
+    public void setNombreProveedor(String nombreProveedor) {this.nombreProveedor = nombreProveedor;}
+
+    public String getTelefonoProveedor() {return telefonoProveedor;}
+
+    public void setTelefonoProveedor(String telefonoProveedor) {this.telefonoProveedor = telefonoProveedor;}
+
+    public String getDireccionProveedor() {return direccionProveedor;}
+
+    public void setDireccionProveedor(String direccionProveedor) {this.direccionProveedor = direccionProveedor;}
+
+    public String getEmailProveedor() {return emailProveedor;}
+
+    public void setEmailProveedor(String emailProveedor) {this.emailProveedor = emailProveedor;}
+}
