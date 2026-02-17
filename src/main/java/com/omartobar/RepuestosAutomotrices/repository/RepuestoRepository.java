a6c85fa0625f0379cdd8c8700bd5dfde52f8de8b
omartobar-2025092 <otobar-2025092@kinal.edu.gt>
2026-02-17 07:34:41 -0600

    refactor: Se hicieron cambios en el repositorio de Repuesto

--- src/main/java/com/omartobar/RepuestosAutomotrices/repository/RepuestoRepository.java
+++ src/main/java/com/omartobar/RepuestosAutomotrices/repository/RepuestoRepository.java
@@ -1,11 +1,10 @@
 package com.omartobar.RepuestosAutomotrices.repository;
 
-import com.omartobar.RepuestosAutomotrices.entity.Empleado;
 import com.omartobar.RepuestosAutomotrices.entity.Repuesto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RepuestoRepository extends JpaRepository<Repuesto, Integer> {
-    
+
 }
